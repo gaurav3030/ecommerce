65ba2e01c42ffa6cfd7b6d9cffea00fed272b0a7
GAURAV <gauravpatil8433@gmail.com>
2020-09-20 17:20:39 +0530

    Updates

---
+++ precache-manifest.142cdb7baae452fd2e3be889c72aee67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd218094065a48d91e12a4e460322897",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "953c6ae13efd0389c361",
+    "url": "/ecommerce/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69e625f3dd87987da132",
+    "url": "/ecommerce/static/js/2.b1ca64b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce/static/js/2.b1ca64b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953c6ae13efd0389c361",
+    "url": "/ecommerce/static/js/main.e63a868d.chunk.js"
+  },
+  {
+    "revision": "831a87ab3375c7c427c2",
+    "url": "/ecommerce/static/js/runtime-main.ee40b3dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
