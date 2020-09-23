0e92a84ea4c6a959036591a27378bcccd102cae7
GAURAV <gauravpatil8433@gmail.com>
2020-09-23 23:46:43 +0530

    login register complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React , {useContext} from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import UserContext from '../../context/UserContext'
 
 function Header() {
+  const {userData , setUserData} = useContext(UserContext);
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined
+    });
+    localStorage.setItem("auth-token","");
+  }
   return (
     <div className="header">
         <div className="header-left">
@@ -13,7 +22,12 @@ function Header() {
         
         <div className="header-right">
             <div className="header-button">CART</div>
-            <Link to="/login"><div className="header-button">LOGIN</div></Link>
+            {
+              userData.user ? <div className="header-button" onClick={logout}>LOGOUT</div> :
+                <Link to="/login"><div className="header-button">LOGIN</div></Link>
+              
+            }
+            
         </div>
     </div>
   );
--- src/pages/AuthPage/Register.js
+++ src/pages/AuthPage/Register.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState , useContext} from 'react';
+import { Link , useHistory } from 'react-router-dom';
 import './Auth.css';
+import Axios from 'axios';
 import Header from '../../components/Header/Header';
+import UserContext from '../../context/UserContext';
+
+
 export default function Register() {
+    const [email , setEmail] = useState();
+    const [password , setPassword] = useState();
+    const [passwordCheck , setPasswordCheck] = useState();
+    const [displayName , setDsiplayName] = useState();
+    
+    const {setUserData} = useContext(UserContext);
+    const history = useHistory();
+    const submit =  async  (e) =>{
+        e.preventDefault();
+        const newUser = {email , password , passwordCheck , displayName};
+        await Axios.post("http://localhost:5000/users/register/",newUser);
+        
+        const loginRes = await Axios.post("http://localhost:5000/users/login/",
+            {email,password}
+        );
+        setUserData({
+            token: loginRes.data.token,
+            user: loginRes.data.user,
+        });
+        localStorage.setItem("auth-token", loginRes.data.token);
+        history.push("/");
+    }
     return (
         <>
         <Link to="/"><Header /></Link>
         <div className="auth-page">
             <h2>Register </h2>
-            <form>
+            <form onSubmit={submit}>
                 <label htmlFor="register-email">Email</label>
-                <input id="register-email" type="email"/>
+                <input id="register-email" type="email" onChange={e=> setEmail(e.target.value)}/>
                 <label htmlFor="register-pass">Password</label>
-                <input id="register-pass" type="password"/>
+                <input id="register-pass" type="password" onChange={e=> setPassword(e.target.value)}/>
                 <label htmlFor="register-cpass">Comfirm Password</label>
-                <input id="register-cpass" type="password"/>
+                <input id="register-cpass" type="password" onChange={e=> setPasswordCheck(e.target.value)}/>
                 <label htmlFor="register-name">Username</label>
-                <input id="register-name" type="text"/>
+                <input id="register-name" type="text" onChange={e=> setDsiplayName(e.target.value)}/>
                 <input type="submit" value="Register"/>
             </form>
             
